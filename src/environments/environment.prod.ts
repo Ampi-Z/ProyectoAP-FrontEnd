e1478d3cc4fa52c58c81e955183cb779c5f8e556
Ampi-Z <azanardi@fi.uba.ar>
2023-05-16 23:46:51 -0300

    -

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 export const environment = {
+    firebase: {
+      projectId: 'portfolio-amz',
+      appId: '1:131362134739:web:4cadc71f6977a09be2d226',
+      storageBucket: 'portfolio-amz.appspot.com',
+//      locationId: 'southamerica-east1',
+      apiKey: 'AIzaSyDtzrdTwRCiQYQrE-CaFz2cRC4UzenTpPA',
+      authDomain: 'portfolio-amz.firebaseapp.com',
+      messagingSenderId: '131362134739',
+    },
     production: true,
     URL: "https://backendamz.onrender.com/"
   };
+  
   
