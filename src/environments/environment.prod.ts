d24a98584b721bf17c8a1122e88b4e7d07289d0d
Ampi-Z <azanardi@fi.uba.ar>
2023-05-16 23:52:58 -0300

    -

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
       projectId: 'portfolio-amz',
       appId: '1:131362134739:web:4cadc71f6977a09be2d226',
       storageBucket: 'portfolio-amz.appspot.com',
-//      locationId: 'southamerica-east1',
+      locationId: 'southamerica-east1',
       apiKey: 'AIzaSyDtzrdTwRCiQYQrE-CaFz2cRC4UzenTpPA',
       authDomain: 'portfolio-amz.firebaseapp.com',
       messagingSenderId: '131362134739',
